6da1fc21f6fddff26491f7c1d6748c9da8b666ca
Eirik Th S <eirik@flowtivity.io>
2021-10-21 00:59:54 +0200

    Glemte en fil

---
+++ src/medlem.php
@@ -0,0 +1,181 @@
+<?php
+
+$medlemmer[] = array(
+    "slug"=>"johannes",
+    "navn"=>"Johannes",
+    "fullt_navn"=>"Johannes Birkeland",
+    "bilde"=>"//media.discordapp.net/attachments/885832319825485845/892078569667297311/johannes.jpg",
+    "linkedIn"=>"https://www.linkedin.com/in/johannes-birkeland-a80405191/",
+    "beskrivelse"=>
+        "20 år fra ingenmannsland (Hovsherad) og bor i Kvadraturen med samboeren min.
+        På VGS ble jeg introdusert til web programmering og databaser. 
+        Etter det søkte jeg på IT og informasjonssystemer som jeg nå til våren 2021 skal skrive bachelor oppgave i. 
+        Gjennom studiet har vi vært gjennom alle mulige felt av IT, men har endt opp med å ha sansen
+        for backend utvikling.
+        Jeg syntes at det er fascinerende å se hvordan data flyter gjennom flere lag med logikk, 
+        og hvordan man kan sette individuelle komponenter sammen slik det blir et system. 
+        Ved siden av studiene så jobber jeg som sikkerhetsanalytiker og hjelpelærer, og på fritiden liker jeg å tegne, 
+        gå tur i naturen, diskutere/spille musikk og kode."
+);
+
+$medlemmer[] = array(
+    "slug"=>"toringe",
+    "navn"=>"Tor Inge",
+    "fullt_navn"=>"Tor Inge Brekka",
+    "bilde"=>"images/tor_inge_crop.png",
+    "linkedIn"=>"https://www.linkedin.com/in/tor-inge-brekka-023a60197/",
+    "beskrivelse"=>"Jeg heter Tor Inge, er 22 år gammel, kommer fra Gjerstad og bor på Vegårshei. På fritiden liker å 
+                        spille gitar, trommer og piano, og være med kjæreste og hund. Etter studiespesialisering jobbet jeg 
+                        et år før jeg begynte å innse min interesse for IT. Jeg går nå siste året på bacheloren 
+                        IT og Informasjonssystemer ved UiA og er glad jeg bestemte meg for å søke på IT linja. 
+                        Et stykke inn i femte semester sitter jeg igjen med varierte og gode kunnskaper innen 
+                        flere områder, men kanskje mest kunnskap og interesse har jeg for systemutvikling gjennom 
+                        tjenestedesign."
+);
+
+$medlemmer[] = array(
+    "slug"=>"anders",
+    "navn"=>"Anders",
+    "fullt_navn"=>"Anders Korsnes",
+    "bilde"=>"images/anders.jpg",
+    "linkedIn"=>"https://www.linkedin.com/in/anders-korsnes-536aa314b/",
+    "beskrivelse"=>""
+);
+
+$medlemmer[] = array(
+    "slug"=>"thomas",
+    "navn"=>"Thomas",
+    "fullt_navn"=>"Thomas Låg Hjelleseth",
+    "bilde"=>"images/thomas_crop.png",
+    "linkedIn"=>"https://www.linkedin.com/in/thomas-l%C3%A5g-04125614b/",
+    "beskrivelse"=>""
+);
+
+$medlemmer[] = array(
+    "slug"=>"bendix",
+    "navn"=>"Bendix",
+    "fullt_navn"=>"Bendix Melkeraaen Vagle",
+    "bilde"=>"//media.discordapp.net/attachments/885832319825485845/891766144531333141/bendix.jpg",
+    "linkedIn"=>"https://www.linkedin.com/in/bendix-vagle-105464a9/",
+    "beskrivelse"=>""
+);
+
+$medlemmer[] = array(
+    "slug"=>"eirik",
+    "navn"=>"Eirik",
+    "fullt_navn"=>"Eirik Thilesen Svagård",
+    "bilde"=>"//useg.it/avatars/18ff2381c713b89a7237786972b86ce7",
+    "linkedIn"=>"https://www.linkedin.com/in/eirik-svagard/",
+    "beskrivelse"=>""
+);
+$medlemmer[] = array(
+    "slug"=>"espen",
+    "navn"=>"Espen",
+    "fullt_navn"=>"Espen Land Stokkeland",
+    "bilde"=>"images/espen_crop.PNG",
+    "linkedIn"=>"https://www.linkedin.com/in/espen-land-stokkeland-736106116/",
+    "beskrivelse"=>"Jeg heter Espen, er 26 år gammel, kommer fra Vennesla og bor i Kristiansand. Etter studiespesialisering gikk jeg på bibelskole, 
+                            elektro og deretter bestemte jeg meg for å gå all in for IT.  Nå er jeg på siste året på Bacheloren ved UiA. 
+                            Nå i semester fem har jeg praksis hos KnowIT, hvor jeg jobber med frontend (React med typescript), men jeg trives godt med backend og database.
+                            På fritiden er jeg glad i å bruke tid ute, game litt, og besøke familien hjemme i Vennesla. Da får jeg også møtt hunden min, som ikke er tillatt
+                            i leiligheten her jeg nå bor."
+);
+
+
+if(isset($included)){
+    return;
+}
+if(!isset($_GET['m'])){
+    header("Location: ./");
+    die();
+}
+
+foreach ($medlemmer as $k => $m){
+    if($m['slug'] == $_GET['m']){
+        $medlem = $medlemmer[$k];
+        break;
+    }
+}
+
+if(!isset($medlem)){
+    header("Location: ./");
+    die();
+}
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ASCII - Portal</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, inital-scale=1.0">
+    <link rel="stylesheet" href="css/medlem.css">
+    <link href="//cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="//cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
+            integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"
+            integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+</head>
+<body>
+<section id="nav-bar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./">ASCII</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./#about">Om gruppa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./#medlemmer">Medlemmer</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./#fokus">Mål og Fokus</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./#kontakt">Kontakt oss</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</section>
+
+<section id="content">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img class="img" id="image" src="<?=$medlem['bilde'];?>" alt="<?=$medlem['navn'];?>">
+            </div>
+            <div class="col">
+                <h1 class="title">
+                    <?=$medlem['fullt_navn'];?>
+                </h1>
+                <p class="text">
+                    <?=$medlem['beskrivelse'];?>
+                </p><br>
+                <a class="linkedin" href="<?=$medlem['linkedIn'];?>" target="_blank">LinkedIn</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="kontakt">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 footer-title">
+                <h1 id="footer-title">Kontakt oss</h1>
+            </div>
+            <div class="col-md-6 footer-text">
+                <h1 id="footer-text">Johannes Birkeland: johannesbi@uia.no</h1>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
